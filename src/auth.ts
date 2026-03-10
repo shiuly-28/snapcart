0dd257170ee7157e207475be535b8d79a33804cd
shiuly-28 <shulybd1245@gmail.com>
2026-03-10 11:56:34 +0600

    credentials error fixed

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,48 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import connectDb from "./lib/db";
+import User from "./models/user.models";
+import bcrypt from "bcryptjs";
+
 
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+        credentials:{
+            email: {label: "email", type: "email"},
+            password: {label: "Password", type: "password"},
+        },
+        async authorize(credentials, request){
+          
+            await connectDb()
+            const email=credentials.email
+            const password=credentials.password as string
+            const user=await User.findOne({email})
+            if(!user){
+              throw new Error("user does not exist")
+            }
+            const isMath=await bcrypt.compare(password, user.password)
+            if(!isMath){
+              throw new Error("incorrecet password")
+            }
+            return{
+              id:user._id,
+              email:user.email,
+              name:user.name,
+              role:user.role
+            }
+
+          
+
+        },
+    })
+  ],
+
+  callbacks:{
+    // token
+    jwt(params){
+
+    },
+  }
 })
