1404f43ee7b84678c73a5d3a26694d674b0817f7
shiuly-28 <shulybd1245@gmail.com>
2026-03-13 21:20:20 +0600

    login page authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+"use client"
+
 import type { Metadata } from "next";
 
 import "./globals.css";
-
 
 
 export const metadata: Metadata = {
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,43 +1,41 @@
+"use client"
+
 import { ArrowLeft, EyeIcon, EyeOff, Leaf, Loader2, Lock, LogIn, Mail, User } from 'lucide-react';
-import React, { useState } from 'react';
+
 import { motion } from "motion/react"
 import Image from 'next/image';
 import googleImage from "@/assests/google.png"
 import axios from 'axios';
+import {  FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
 
 
-type propType={
-  previousStep:(s:number)=>void
-}
 
-
-const RegisterFrom = ({previousStep}:propType) => {
-  const [name, setName]=useState("")
+const Login = () => {
+  
   const [email, setEmail]=useState("")
   const [password, setPassword]=useState("")
   const [showPassword, setShowPassword]=useState(false)
   const [loading, setLoading]=useState(false)
   const router=useRouter()
-  const handleRegister=async (e:React.FormEvent)=>{
+  const handleLogin = async(e:FormEvent)=>{
     e.preventDefault()
     setLoading(true)
     try{
-      const result=await axios.post("/api/auth/register",{
-        name, email, password
+      await signIn("credentials",{
+        email,password
       })
-      console.log(result.data)
-      setLoading(false)
     }catch(error){
       console.log(error)
       setLoading(false)
-    };
-    
+    }
   }
+ 
     return (
         <div className='flex flex-col items-center justify-center min-h-screen px-6 bg-white relative'>
             <div className='absolute top-6 left-6 flex items-center gap-2 text-green-600 hover:text-green-800 cursor-pointer'
-            onClick={()=>previousStep(1)}
+        
             >
                 <ArrowLeft className='w-s h-s'/>
                 <span className='font-medium'>Back</span>
@@ -56,12 +54,12 @@ const RegisterFrom = ({previousStep}:propType) => {
         delay:0.8
       }}
             className='text-4xl font-extrabold text-green-700 mb-2'>
-                Create Account
+                Welcome back
             </motion.h1>
-            <p className='flex text-gray-500 mt-5 items-center'>Join Snapcart
+            <p className='flex text-gray-500 mt-5 items-center'>Login To Snapcart Today
                  Today <Leaf className='w-5 h-5 text-green-600'/></p>
                  <motion.form 
-                 onSubmit={handleRegister}
+                onSubmit={handleLogin}
                   initial={{
         opacity:0,
         y:20
@@ -74,13 +72,7 @@ const RegisterFrom = ({previousStep}:propType) => {
         duration:0.6,
         delay:0.8
       }}className='flex flex-col gap-5 w-full max-w-sm'>
-        <div className='relative'>
-        <User className='absolute left-3 top-3.5 text-gray-400 w-s h-s hover:text-gray-400 '/>
-        <input type="text" placeholder='Your Name' className='w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:ring-2 focus:ring-green-500 focus:outline-none' 
-        onChange={(e)=>setName(e.target.value)}
-        value={name}
-        />
-        </div>
+     
         <div className='relative'>
         <Mail className='absolute left-3 top-3.5 text-gray-400 w-s h-s hover:text-gray-400 '/>
         <input type="text" placeholder='Your Email' className='w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:ring-2 focus:ring-green-500 focus:outline-none' 
@@ -103,14 +95,14 @@ const RegisterFrom = ({previousStep}:propType) => {
 
         {
           (()=>{
-            const formValidation=name!=="" && email!=="" && password!==""
+            const formValidation= email!=="" && password!==""
             return<button disabled = {!formValidation || loading}
             className={`w-full font-semibold py-3 rounded-xl transition-all duration-200 shadow-md inline-flex items-center justify-center gap-2{
               formValidation
               ? " bg-green-600 hover:bg-green-700 text-white"
               :"bg-gray-300 text-white cursor-not-allowed"
               }`}>
-                {loading?<Loader2 className='w-4 h-4 animate-ping'/>:"Register"}
+                {loading?<Loader2 className='w-4 h-4 animate-ping'/>:"Login"}
                 </button>
           })()
         }
@@ -125,10 +117,10 @@ const RegisterFrom = ({previousStep}:propType) => {
           Continue width Google
         </button>
       </motion.form>
-      <p className='cursor-pointer text-gray-600 mt-6 flex text-sm items-center gap-1'onClick={()=>router.push("/login")}>Already have an account ? 
-        <LogIn className='w-4 h-4'/> <span className='text-green-700'>Sign in</span></p>
+      <p className='cursor-pointer text-gray-600 mt-6 flex text-sm items-center gap-1' onClick={()=> router.push("/register")}>Want to create an account ? 
+        <LogIn className='w-4 h-4'/> <span className='text-green-700'>Sign Up</span></p>
         </div>
     );
 };
 
-export default RegisterFrom;
+export default Login;
