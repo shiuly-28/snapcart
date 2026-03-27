c7cf401bce86d0cc32abb735aab4576246ecb471
shiuly-28 <shulybd1245@gmail.com>
2026-03-27 20:25:53 +0600

    navbnar page done

--- src/components/Navber.tsx
+++ src/components/Navber.tsx
@@ -1,12 +1,13 @@
 "use client";
-import { LogOut, Package, Search, ShoppingBag, ShoppingCartIcon, User } from 'lucide-react'
+import { Cross, LogOut, Package, Search, ShoppingBag, ShoppingCartIcon, User, X } from 'lucide-react'
 import mongoose from 'mongoose'
 import { AnimatePresence } from 'motion/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { motion } from 'framer-motion';
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { signOut } from 'next-auth/react';
+
 interface IUser{
     _id:mongoose.Types.ObjectId
     name:string
@@ -20,13 +21,25 @@ interface IUser{
 function Navber({user}:{user:IUser}) {
     // console.log(user)
     const [open, setOpen]=useState(false)
+    const ProfileDropDown=useRef<HTMLDivElement>(null)
+    const [searchberOpen, setSearchBarOpen]=useState(false)
+
+    useEffect(()=>{
+      const handleClickOutside=(e:MouseEvent)=>{
+        if(ProfileDropDown.current && !ProfileDropDown.current.contains(e.target as Node)){
+           setOpen(false)
+        }
+         
+      }
+      document.addEventListener("mousedown", handleClickOutside)
+      return()=>document.removeEventListener("mousedown", handleClickOutside )
+    },[])
   return (
     <div className='w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-orange-300 to-orange-500 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-20 px-4 md:px z-50'> 
       <Link href={"/"} className='text-white font-extrabold text-2xl sm:text-3xl tracking-wide hover:scale-105 transitions-transform'>
       SwiftPick
       </Link>
     <div className="flex items-center w-full px-4 py-2">
-  {/* ml-auto সার্চ বারকে ডান পাশে নিয়ে যাবে */}
   <form className='hidden md:flex items-center bg-white rounded-full px-4 py-2 max-w-lg shadow-md ml-auto border border-gray-100'>
     <Search className='text-gray-500 w-5 h-5 mr-2' />
     <input 
@@ -37,11 +50,16 @@ function Navber({user}:{user:IUser}) {
   </form>
 </div>
       <div className='flex items-center gap-3 md:gap-6 relative'>
+        <div className='bg-white rounded-full w-10 h-10 flex items-center justify-center shadow-md hover:scale-105 transtion md:hidden'
+        onClick={()=>setSearchBarOpen((prev)=>!prev)}
+        >
+        <Search className='text-amber-500 w-6 h-6'/>
+        </div>
     <Link href={""} className='relative bg-white rounded-full w-11 flex items-center justify-center shadow-md hover:scale-105 transition'>
     <ShoppingCartIcon className='text-orange-600 w-6 h-6'/>
     <span className='absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center font-semibold shadow rounded-full'>0</span>
     </Link>
-    <div className='relative'>
+    <div className='relative' ref={ProfileDropDown}>
     <div className='bg-white rounded-full  w-11 h-11 flex items-center justify-center overflow-hidden shadow-md hover:scale-105 transition-transform ' 
     onClick={()=>setOpen(prev=>!prev)}>
       {user.image?<Image src={user.image} alt='user' fill className='object-cover rounded-full'/>:<User/>}
@@ -50,7 +68,7 @@ function Navber({user}:{user:IUser}) {
       {open && <motion.div
       initial={{opacity:0, y: -10, scale: 0.95 }} 
       animate={{opacity:1, y: 0, scale: 1}}
-      transition={{duration:0.6}}
+      transition={{duration:0.4}}
       exit={{opacity:0, y: -10, scale: 0.95}}
       className='absolute right-0 mt-3 w-56 bg-white rounded-2xl shadow-2xl border  border-gray-200 p-3 z-999'
       >
@@ -80,6 +98,29 @@ function Navber({user}:{user:IUser}) {
         </motion.div>
         }
 
+    </AnimatePresence>
+    <AnimatePresence>
+      {
+        searchberOpen
+        &&
+        <motion.div
+        initial={{opacity:0, y: -10, scale: 0.95 }} 
+      animate={{opacity:1, y: 0, scale: 1}}
+      transition={{duration:0.4}}
+      exit={{opacity:0, y: -10, scale: 0.95}}
+      className='fixed top-24 left-1/2 -translate-x-1/2 w-[90%] bg-white rounded-full shadow-lg z-40 flex items-center px-4 py-2'
+
+        >
+          <Search className='text-gray-500 w-5 h-5 mr-2'/>
+          <form className='grow'>
+            <input type="text" className='w-full outline-none text-gray-600'/>
+          </form>
+          <button onClick={()=>setSearchBarOpen(false)}>
+            <X className='text-gray-500 w-5 h-5'/>
+          </button>
+
+        </motion.div>
+      }
     </AnimatePresence>
     </div>
       </div>
