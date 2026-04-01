5fd508a4b9a4747df7d46799f7a12cfa5d5f2796
shiuly-28 <shulybd1245@gmail.com>
2026-04-01 18:12:32 +0600

    resolve React warning for synchronous setState within useEffect in CategorySlider

--- src/components/UsersDashboard.tsx
+++ src/components/UsersDashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from './HeroSection';
+import CategorySlider from './CategorySlider';
 
 const UsersDashboard = () => {
     return (
         <div>
             <HeroSection/>
+            <CategorySlider/>
         </div>
     );
 };
