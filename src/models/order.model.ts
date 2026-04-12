42c91d2e8c9bb5a62175c73e57145ec10e85c3ad
shiuly-28 <shulybd1245@gmail.com>
2026-04-12 19:41:42 +0600

    order model implement done

---
+++ src/models/order.model.ts
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+
+
+interface IOrder{
+    _id:mongoose.Types.ObjectId
+    user:mongoose.Types.ObjectId
+    items:[
+        {
+            grocery:mongoose.Types.ObjectId,
+            name:string,
+            price:string,
+            unit:string,
+            image:string
+            quantity:number
+        }
+    ]
+    totalAmount:string,
+    paymentMethod:"cod" | "online"
+    address:{
+        fullName:string,
+        mobile:string,
+        city:string,
+        state:string,
+        pincode:string,
+        fullAddress:string,
+        latitude:number,
+        longitude:number
+    }
+    status:"pending" | "out of delivery" | "delivered",
+    createdAt?:Date
+    updated?:Date
+}
+
+const orderSchema=new mongoose.Schema<IOrder>({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    items:[
+        {
+            grocery:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Grocery",
+                required:true
+            },
+            name:String,
+            price:String,
+            unit:String,
+            image:String,
+            quantity:Number
+        }
+    ],
+    paymentMethod:{
+        type:String,
+        enum:["cod", "online"],
+        default:"cod"
+    },
+    address:{
+         fullName:String,
+        mobile:String,
+        city:String,
+        state:String,
+        pincode:String,
+        fullAddress:String,
+        latitude:Number,
+        longitude:Number
+    },
+    status:{
+        type:String,
+        enum:["pending", "out of delivery", "delivered" ],
+        default:"pending"
+    }
+},{timestamps:true})
+
+const Order=mongoose.models.Order || mongoose.model("Order", orderSchema)
+export default Order
